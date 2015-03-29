b8005f829809539c61d84fe1cb2069136ebbbef4
Jacob Grevald <jacob@jacobgrevald.dk>
2015-03-29 17:44:20 +0800

    Remove \r, tab-indent

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -1,52 +1,52 @@
 public class PlayerSkeleton {
 
-  //implement this function to have a working system
-  public int pickMove(State s, int[][] legalMoves) {
-    return 0;
-  }
-
-  public double heuristic(State s, double[] weights) {
-	double sum = 0.0;
-	int[] top = s.getTop();
-	int[][] field = s.getField();
-	int wi = 0;
-
-	int maxHeight = 0;
-	for(int i = 0; i < top.length; i++) {
-		sum += top[i] * weights[wi++];
-		if(top[i] > maxHeight) maxHeight = top[i];
+	//implement this function to have a working system
+	public int pickMove(State s, int[][] legalMoves) {
+		return 0;
 	}
 
-	for(int i = 0; i < top.length - 1; i++){ sum += Math.abs(top[i] - top[i+1]) * weights[wi++];
-	}
+	public double heuristic(State s, double[] weights) {
+		double sum = 0.0;
+		int[] top = s.getTop();
+		int[][] field = s.getField();
+		int wi = 0;
 
-	int holes = 0;
-	for(int col = 0; col < field[0].length; col++){
-		for(int row = 0; row < top[col]; row++){
-			if(field[row][col] == 0) holes++;
+		int maxHeight = 0;
+		for(int i = 0; i < top.length; i++) {
+			sum += top[i] * weights[wi++];
+			if(top[i] > maxHeight) maxHeight = top[i];
 		}
+
+		for(int i = 0; i < top.length - 1; i++) {
+			sum += Math.abs(top[i] - top[i+1]) * weights[wi++];
+		}
+
+		int holes = 0;
+		for(int col = 0; col < field[0].length; col++) {
+			for(int row = 0; row < top[col]; row++) {
+				if(field[row][col] == 0) holes++;
+			}
+		}
+
+		sum += holes * weights[wi++];
+
+		return sum;
 	}
 
-	sum += holes * weights[wi++];
-			
-	return sum;
-  }
-
-  public static void main(String[] args) {
-    State s = new State();
-    new TFrame(s);
-    PlayerSkeleton p = new PlayerSkeleton();
-    while(!s.hasLost()) {
-      s.makeMove(p.pickMove(s,s.legalMoves()));
-      s.draw();
-      s.drawNext(0,0);
-      try {
-        Thread.sleep(300);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    }
-    System.out.println("You have completed "+s.getRowsCleared()+" rows.");
-  }
-
+	public static void main(String[] args) {
+		State s = new State();
+		new TFrame(s);
+		PlayerSkeleton p = new PlayerSkeleton();
+		while(!s.hasLost()) {
+			s.makeMove(p.pickMove(s,s.legalMoves()));
+			s.draw();
+			s.drawNext(0,0);
+			try {
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("You have completed "+s.getRowsCleared()+" rows.");
+	}
 }
